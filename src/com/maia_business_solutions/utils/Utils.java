1d496786d5cada9247b66dfae293ea357c85aea9
Adrian Q Montgomery <aqm1970@yahoo.com>
2013-03-24 12:40:59 -0400

    Add ability to copy files to external storage for use by other applications

--- src/com/maia_business_solutions/utils/Utils.java
+++ src/com/maia_business_solutions/utils/Utils.java
@@ -1,11 +1,18 @@
 package com.maia_business_solutions.utils;
 
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import android.content.Context;
 import android.util.Log;
 
-public class Utils {
+final public class Utils {
+  private static final String NAME = Utils.class.getSimpleName();
+  
   public static void close(Closeable closeable)
   {
     if (closeable != null) {
@@ -16,5 +23,39 @@ public class Utils {
         Log.w(Utils.class.getSimpleName(), e);
       }
     }
+  }
+  
+  public static File copyToExternal(final Context context,
+      final InputStream inputStream, final String file)
+  {
+    File retVal = null;
+    
+    OutputStream outStream = null;
+    
+    try {
+      final File outFile = new File(context.getExternalFilesDir(null), file);
+      
+      if (outFile.exists() == false) {
+        Log.d(NAME, "File (" + outFile.getAbsolutePath() + ") did not exist." +
+        		" Copying to getExternalFilesDir(null)");
+        
+        outStream = new FileOutputStream(outFile);
+        final byte[] data = new byte[inputStream.available()];
+
+        inputStream.read(data);
+        outStream.write(data);
+      }
+      
+      retVal = outFile;
+    }
+    catch (Exception e) {
+      Log.e(NAME, "Utils.copyToExternal", e);
+    }
+    finally {
+      close(inputStream);
+      close(outStream);
+    }
+    
+    return retVal;
   }
 }
