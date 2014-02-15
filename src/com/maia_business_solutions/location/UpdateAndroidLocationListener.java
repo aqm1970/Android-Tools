d842e0449c245b4aae7147dfc45a0d068d955fb0
Adrian Q Montgomery <aqm1970@yahoo.com>
2014-02-15 10:39:41 -0500

    Initial Android Location Listener wrapper

---
+++ src/com/maia_business_solutions/location/UpdateAndroidLocationListener.java
@@ -0,0 +1,8 @@
+package com.maia_business_solutions.location;
+
+import android.location.Location;
+
+public interface UpdateAndroidLocationListener
+{
+  public void update(final Location newLocation);
+}
