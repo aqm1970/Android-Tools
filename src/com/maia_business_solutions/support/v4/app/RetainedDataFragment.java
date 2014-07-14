fd03ca432ae44729d9775478225e4f93661b2cb4
Adrian Quentin Montgomery <aqm1970@yahoo.com>
2014-07-13 21:37:34 -0400

    Changes to RetainedDataFragment

--- src/com/maia_business_solutions/support/v4/app/RetainedDataFragment.java
+++ src/com/maia_business_solutions/support/v4/app/RetainedDataFragment.java
@@ -2,10 +2,12 @@ package com.maia_business_solutions.support.v4.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 public class RetainedDataFragment extends Fragment
 {
-  public static final String TAG = RetainedDataFragment.class.getCanonicalName();
+  private static final String TAG = RetainedDataFragment.class.getCanonicalName();
   
   private final Bundle dataBundle = new Bundle();
   
@@ -20,5 +22,22 @@ public class RetainedDataFragment extends Fragment
   public Bundle getDataBundle()
   {
     return dataBundle;
+  }
+  
+  public static RetainedDataFragment getDataFragment(final FragmentManager fm)
+  {
+    RetainedDataFragment retVal = null;
+    
+    retVal = (RetainedDataFragment) fm.findFragmentByTag(TAG);
+    
+    if (retVal == null) {
+      retVal = new RetainedDataFragment();
+      
+      final FragmentTransaction ft = fm.beginTransaction();
+      ft.add(retVal, TAG);
+      ft.commit();
+    }
+    
+    return retVal;
   }
 }
