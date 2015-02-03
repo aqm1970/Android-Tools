74ce07eb86df3e35c1b1fd7ad313262fa9bc08dd
Adrian Q Montgomery <aqm1970@yahoo.com>
2015-02-03 18:29:25 -0500

    Temporary fix for the AdView bug

--- src/com/maia_business_solutions/AdFragment.java
+++ src/com/maia_business_solutions/AdFragment.java
@@ -1,7 +1,9 @@
 package com.maia_business_solutions;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,23 @@ import com.maia_business_solutions.tools.R;
 
 public class AdFragment extends Fragment
 {
+  private static final String LOG_TAG = AdFragment.class.getSimpleName();
+  
   private AdView adView;
+
+  @Override
+  public void onAttach(Activity activity)
+  {
+    super.onAttach(activity);
+    
+    if (adView == null) {
+      adView = new AdView(activity);
+      
+      Log.i(LOG_TAG, "Instantiated 'adView'");
+    }
+    
+    Log.i(LOG_TAG, "onAttach");
+  }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,8 +40,6 @@ public class AdFragment extends Fragment
   {
     final ViewGroup group = (ViewGroup) inflater.inflate(
         R.layout.ad_frame, container, false);
-    
-    adView = new AdView(getActivity());
     
     adView.setAdSize(AdSize.SMART_BANNER);
     adView.setAdUnitId("ca-app-pub-5976313619192225/9481126998");
@@ -54,32 +70,38 @@ public class AdFragment extends Fragment
     return group;
   }
 
-  @Override
-  public void onDestroy()
+  public void onDestroyActivity()
   {
-    if (adView != null)
+    if (adView != null) {
       adView.destroy();
+      
+      Log.i(LOG_TAG, "Destroyed 'adView'");
+    }
     
     adView = null;
     
-    super.onDestroy();
+    Log.i(LOG_TAG, "onDestroyActivity");
   }
 
-  @Override
-  public void onPause()
+  public void onPauseActivity()
   {
-    super.onPause();
-    
-    if (adView != null)
+    if (adView != null) {
       adView.pause();
+      
+      Log.i(LOG_TAG, "Paused 'adView'");
+    }
+    
+    Log.i(LOG_TAG, "onPauseActivity");
   }
 
-  @Override
-  public void onResume()
+  public void onResumeActivity()
   {
-    super.onResume();
-    
-    if (adView != null)
+    if (adView != null) {
       adView.resume();
+      
+      Log.i(LOG_TAG, "Resumed 'adView'");
+    }
+    
+    Log.i(LOG_TAG, "onResumeActivity");
   }
 }
